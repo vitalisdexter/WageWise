30c1938496515eedd786f7031b307046975259fa
Vitalis Nwabueze <37216574+vitalisdexter@users.noreply.github.com>
2026-06-19 18:53:39 +0100

    Update pdf.js
    pdf export feature updated

--- js/pdf.js
+++ js/pdf.js
@@ -1,1 +1,26 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+async function downloadPDF(){
+
+const { jsPDF } = window.jspdf;
+
+const pdf =
+new jsPDF();
+
+pdf.text(
+"WageWise Payslip",
+20,
+20
+);
+
+pdf.text(
+document
+.getElementById(
+"payslipContainer"
+).innerText,
+20,
+40
+);
+
+pdf.save(
+"payslip.pdf"
+);
+}
